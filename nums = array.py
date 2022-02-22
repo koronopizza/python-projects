6833ce67d2504240a7a71e64df7b111db35821a1
will <88606984+koronopizza@users.noreply.github.com>
2022-02-21 23:33:37 -0600

    Update nums = array.py

--- nums = array.py
+++ nums = array.py
@@ -1,6 +1,11 @@
-nums = [10,9,8,7,6,5]
-nums[0] = nums[1]-5
+nums = [10,9,8,7,6,5] # this is an array
+
+nums[0] = nums[1]-5   # this is replacing idex 0 with idex 1, then substracting 5 from that
+
 if 4 in nums:
+    
     print(nums[3])
+
 else:
+    
     print(nums[4])    
