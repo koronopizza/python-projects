4b80e207832b1f142af50e518a99a0e391b79207
will <88606984+koronopizza@users.noreply.github.com>
2022-02-24 23:03:37 -0600

    Update working with ranges.py

--- working with ranges.py
+++ working with ranges.py
@@ -1,7 +1,7 @@
-a = int(input())
+a = int(input())  # if this is 10
 
-b = int(input())
+b = int(input())  # if this is 5
 
-num = list(range(a,b))
+num = list(range(a,b))  # this is a variable with a value of list. this is defining the range
 
-print(num)
+print(num)      
