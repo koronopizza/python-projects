fcbe3260f86d38d14f8cebc5ec0cd4cf3b8ae320
will <88606984+koronopizza@users.noreply.github.com>
2022-02-26 19:36:12 -0600

    Add files via upload

---
+++ working_on_arrays1.py
@@ -0,0 +1,15 @@
+import random 
+
+morning_list = ("eggs","bacon","toast","sauasge","oj","bagel","pancakes")
+
+afternoon_list = ("blt","salad","taco","stuffed peppers","grilled cheese","chicken salad","chicken pitas")
+
+night_list = ("pizza","steak","hotdogs","pork chops","spaghetti","pasta","hamburgers")
+
+x = Random_moring=random.choice(morning_list)
+
+y = Random_afternoon=random.choice(afternoon_list)
+
+z = Random_night=random.choice(night_list)
+
+print(z)
