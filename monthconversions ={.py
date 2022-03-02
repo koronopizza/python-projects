8f58ce74e185ef47c02b11379c74e8d78bf22300
will <88606984+koronopizza@users.noreply.github.com>
2022-03-01 21:09:46 -0600

    Add files via upload

---
+++ monthconversions ={.py
@@ -0,0 +1,17 @@
+monthconversions ={
+    "jan": "january",
+    "feb": "february",
+    "mar":  "march",
+    "apr":  "april",
+    "may":  "may",
+    "jun":  "june",
+    "jul":  "july",
+    "aug":  "august",
+    "sep":  "september",
+    "nov":  "november",
+    "dec":  "december"
+}
+
+print(monthconversions.get("aug"))
+
+print(monthconversions.get("jan"))
