c6ffb0438c34e9006ec9712cd187760bde4da6da
will <88606984+koronopizza@users.noreply.github.com>
2022-03-04 23:09:11 -0600

    Update test .py

--- test .py
+++ test .py
@@ -1,11 +1,11 @@
 i = 0
-# these are the variables    
+ # these are the variables    
 x = "tacocat"
-# this is the count of the variable "tacocat"
+ # this is the count of the variable "tacocat"
 y = len(x)-1 #6
-#this loop compares both sides of the variable    
+ #this loop compares both sides of the variable    
 while i < y + 1 : 
     if x[i] != x[y - i]:
         print('false!')
-        break  # stops the loop
-    i += 1  # increases the count or iteration
+        break   # stops the loop
+    i += 1   # increases the count or iteration
