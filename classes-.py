78fb02ccecf5fc46d5d9f56bb00ce8198ad77842
will <88606984+koronopizza@users.noreply.github.com>
2022-03-16 20:29:08 -0500

    Update classes-.py

--- classes-.py
+++ classes-.py
@@ -1,15 +1,15 @@
 class Person:                                                 # just working with classes
-  def __init__(self, name, age, money):
-    self.name = name
+  def __init__(self, name, age, money):     # this will always be with class    # Parameter 
+    self.name = name        
     self.age = age
     self.money = money
 
   def myfunc(self):
     print("Hello my name is " + self.name)
 
-p1 = Person("Will", 48, 1000)
+p1 = Person("Will", 48, 1000)    #object
 
-print(p1.name)
+print(p1.name)    
 print(p1.age)
 print(p1.money)
 
