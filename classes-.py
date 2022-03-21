62c078801a98329098c945f3d609084ce72a17eb
will <88606984+koronopizza@users.noreply.github.com>
2022-03-21 15:41:17 -0500

    Update classes-.py

--- classes-.py
+++ classes-.py
@@ -1,13 +1,13 @@
 class Person:                                                 # just working with classes
   def __init__(self, name, age, money):     # this will always be with class    # Parameter 
-    self.name = name                       # this is a method
+    self.name = name                        # this is a method
     self.age = age
     self.money = money
 
   def myfunc(self):
     print("Hello my name is " + self.name)
 
-p1 = Person("Will", 48, 1000)    #object
+p1 = Person("Will", 48, 1000)      #object
 
 print(p1.name)    # printing the object name
 print(p1.age)     # printing the object age
