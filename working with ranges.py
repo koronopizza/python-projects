71d4760a38031b5005ca242b976bd6d137d47b23
will <88606984+koronopizza@users.noreply.github.com>
2022-04-02 14:40:40 -0500

    Update working with ranges.py

--- working with ranges.py
+++ working with ranges.py
@@ -1,6 +1,6 @@
 a = int(input())   # if this is 10
 
-b = int(input())  # if this is 5
+b = int(input())   # if this is 5
 
 num = list(range(a,b))  # this is a variable with a value of list. this is defining the range
 
