cc2860f4e5bd2dd28ff3e2a9703911d46b9c0e46
will <88606984+koronopizza@users.noreply.github.com>
2022-04-02 14:41:59 -0500

    Update temp = int(input()) .py

--- temp = int(input()) .py
+++ temp = int(input()) .py
@@ -1,9 +1,9 @@
-temp = int(input())  # takes the temp as input
+temp = int(input())    # takes the temp as input
 
-if temp >= 100 :     # statement condition
+if temp >= 100 :       # statement condition
 
-    print("Boiling")  # if statement true
+    print("Boiling")   # if statement true
 
 else: 
 
-    print("done")    # if statement false
+    print("done")     # if statement false
