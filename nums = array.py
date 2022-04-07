710f229f560e04dd4baccf611d3b1188a470714d
will <88606984+koronopizza@users.noreply.github.com>
2022-04-06 22:07:45 -0500

    Update nums = array.py

--- nums = array.py
+++ nums = array.py
@@ -1,8 +1,8 @@
-nums = [10,9,8,7,6,5] # this is an array
+nums = [10,9,8,7,6,5]  # this is an array
 
-nums[0] = nums[1]-5   # this is replacing idex 0 with idex 1, then substracting 5 from that
+nums[0] = nums[1]-5    # this is replacing idex 0 with idex 1, then substracting 5 from that
 
-if 4 in nums:         # if statement looking for 4 in the array 
+if 4 in nums:          # if statement looking for 4 in the array 
     
     print(nums[3])
 
