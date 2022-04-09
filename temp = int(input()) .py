14e1ff14dc2caf9cb9ed679a0ed5d2dcc347ea4a
will <88606984+koronopizza@users.noreply.github.com>
2022-04-08 23:01:17 -0500

    Update temp = int(input()) .py

--- temp = int(input()) .py
+++ temp = int(input()) .py
@@ -2,7 +2,7 @@ temp = int(input())    # takes the temp as input
 
 if temp >= 100 :       # statement condition
 
-    print("Boiling")    # if statement true
+    print("Boiling")   # if statement true
 
 else: 
 
