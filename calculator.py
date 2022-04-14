0d089ed33e8b7e70267839905bd53715ed411d75
will <88606984+koronopizza@users.noreply.github.com>
2022-04-13 20:07:27 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@ num1 = (input("Enter first number:"))    # this is where you input the numbers
 op = (input("Enter operator:"))          # this is where you input the operator
 num2 = (input("Enter second number:"))   # this is where you input the numbers
 
- # if and else statements for what op you choose
+  # if and else statements for what op you choose
 if op == "+":
     print(num1 + num2)
 elif op == "-":
