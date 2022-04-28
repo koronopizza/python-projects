ea510b04eb60d94ef203920acdaeaee2dba539b1
will <88606984+koronopizza@users.noreply.github.com>
2022-04-28 10:15:32 -0500

    Update test .py

--- test .py
+++ test .py
@@ -3,7 +3,7 @@ i = 0
 x = "tacocat"
    # this is the count of the variable "tacocat"
 y = len(x)-1   #6
-  #this loop compares both sides of the variable    
+   #this loop compares both sides of the variable    
 while i < y + 1 : 
     if x[i] != x[y - i]:
         print('false!')
